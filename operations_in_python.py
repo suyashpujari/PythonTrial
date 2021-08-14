7e8104f770611f7b11aa09b85a9b977aa6aa470d
suyashpujari <62280304+suyashpujari@users.noreply.github.com>
2021-08-14 19:19:03 +0530

    Operations in python

---
+++ operations_in_python.py
@@ -0,0 +1,35 @@
+# 1)-> <>
+# 2)->
+i=3
+j=2
+print("\n2) Ans: i+5>=j-6 =",i+5>=j-6)
+# 3)-> x**y
+# 4)-> //
+# 5)->  1)Parenthesis 
+#       2)Exponentiation
+#       3)Division
+#       4)Multiplication   
+#       5)Addition
+#       6)Subtraction
+# 6)->  
+print("6) Ans :",22%3)
+# 7)-> false
+# 8)-> operators are special symbols that designate that some sort of computation should be performed
+# 9)-> Python Operator falls into 7 categories: 1)Arithmetic Operator
+#                                               2)Relational Operator
+#                                               3)Assignment Operator  
+#                                               4)Logical Operator 
+#                                               5)Membership Operator 
+#                                               6)Identity Operator  
+#                                               7)Bitwise Operator
+
+ 
+ 
+ 
+ 
+
+
+
+
+
+
