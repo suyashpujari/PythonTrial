60f223a026b02136b2333c266817e83e2c6ee0e7
suyashpujari <62280304+suyashpujari@users.noreply.github.com>
2021-08-16 17:31:50 +0530

    practical

---
+++ 16th aug practical.py
@@ -0,0 +1,18 @@
+# 11
+a = 'X'
+print("The ASCII value of X is", ord(a))
+
+a='x'
+print("The ASCII value of x is", ord(a))
+
+# 12 Write Program to Compute Quotient and Remainder
+a=4
+b=8
+print("Quotient: ",a//b)
+print("Remainder",a%b)
+# 13
+# 14
+# 15
+# 16
+# 17
+# 18
