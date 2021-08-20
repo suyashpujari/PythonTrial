f85e6bad80f253dda8d75554dddfbfc6be8e6907
suyashpujari <suyashpujari138@gmail.com>
2021-08-20 12:46:57 +0530

    User Defined Functions in python

---
+++ math_pgm.py
@@ -0,0 +1,15 @@
+from mathamatical_methods.addition.addition import add
+from mathamatical_methods.substraction.substraction import sub
+from mathamatical_methods.multiplication import multiplication as ml
+from mathamatical_methods.division import division as di
+
+a=add(50,70)
+b=sub(90,40)
+c=ml.multi(100,10)
+d=di.div(50,5)
+
+print("\nAddition: ",a)
+print("\nSubstraction: ",b)
+print("\nMultiplication: ",c)
+print("\nDivision: ",d)
+
